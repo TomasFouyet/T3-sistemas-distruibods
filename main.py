e272a8f153590b9e4dfcb79f8fce2d56d45cd03d
TomasFouyet <tfouyet@uc.cl>
2025-10-25 01:27:16 -0300

    Implement initial structure for consultas, read_file, and write_file modules

--- main.py
+++ main.py
@@ -13,25 +13,61 @@ import itertools
 import dataclasses
 import enum
 
-# Recuerda que no se permite importar otros módulos/librerías a excepción de los creados
-# por ustedes o las ya incluidas en este main.py
+from read_file import read_file
+from write_file import write_results
+from consultas import ver_query, query_handler
+
+def main():
+    if len(argv) < 2:
+
+        return
+
+    input_path = argv[1]
+    data = read_file(input_path)
+
+    initial_db: dict[str, str] = data.get("DATA", {})
+    servers: list[str] = data.get("SERVERS", [])
+    validation: str = data.get("VALIDATION", "forward")
+    eventos: list[str] = data.get("TRANSACTIONS", [])
+    print("Inicializando sistema con:")
+    print("- Base de datos inicial:", initial_db)
+    print("- Servidores:", servers)
+    print("- Modo de validación:", validation)
+    print("- Eventos de transacciones:", eventos)
+
+    
+    
+    ## FALTA INSTANCIAR MOTOR DE TRANSACCIONES
+    
+    
+    logs: list[str] = []
+
+    for linea in eventos:
+        
+        line = line.strip()
+        print("Procesando línea:", line)
+        if not line:
+            continue
+
+        if ver_query(line):
+            print("Es una consulta.")
+            out_line = query_handler(line, tm)
+            print("Resultado de la consulta:", out_line)
+            logs.append(out_line)
+        else:
+            print("Es un comando de transacción.")
+            # Comandos de transacciones (Parte A)
+            ## FALTA IMPLEMENTAR EL MANEJADOR DE EVENTOS
+            
+
+    # Salida final
+    ## FALTA OBTENER BASE DE DATOS FINAL Y ESTADÍSTICAS
+    write_results(
+        input_path=input_path,
+        log_lines=logs,
+        ## AGREGAR BASE DE DATOS FINAL Y ESTADÍSTICAS
+    )
+
 
 if __name__ == "__main__":
-    # Completar con tu implementación o crea más archivos y funciones
-    print(argv)
-
-    print("Un, dos, tres y...")
-    print("Mala")
-    print("Esta cuenta es mala")
-    print("Se cae la ventana")
-    print("No cuadra la tabla")
-    print("Mala")
-    print("Como fila muy larga")
-    print("Como clave olvidada")
-    print("Esta cuenta es mala")
-    print("Mala")
-    print("Como tasa muy alta")
-    print("Como deuda atrasada")
-    print("Como sistema caído")
-    print("Mala")
-    print("¡Pero es mía!")
+    main()
